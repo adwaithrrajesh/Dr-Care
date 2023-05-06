c38972100e3b332ee2d8d0240ada892abe91ca8e
Adwaith R Rajesh <adwaithrrajesh.k@gmail.com>
2023-05-06 19:22:30 +0530

    home screen style changed

--- client/src/components/patient/home/DepartmentSideScroll.jsx
+++ client/src/components/patient/home/DepartmentSideScroll.jsx
@@ -35,9 +35,12 @@ const Departments = () => {
 
 
   return (
-    <section className='departmentSection mt-5 '>
+    <section className='departmentSection mt-5'>
     <div className='w-full flex justify-center mt-3'>
-      <h1 className='text-cyan-800 text-3xl font-bold'>Popular Departments</h1></div>
+      <div className="justify-center w-2/3 ">
+      <h1 className='text-cyan-800 text-3xl font-bold'>Top Departments</h1></div>
+      </div>
+  
     <div className="department-container bg-white border rounded-lg p-8">
         <div className="department-list mt-2 text-sm">
             <ul className="list flex justify-center flex-wrap w-1/2">
@@ -48,17 +51,18 @@ const Departments = () => {
               }
             </ul>
         </div>
-        <div className="flex-1 mr-16 justify-start ">
+        
+        <div className="flex-1  justify-center ">
           {
             departmentDetails ?
-            <div className=" flex flex-col ">
+            <div className=" flex flex-col justify-center  ">
                     <div className="department-image mb-8  hidden sm:block">
                         <img src={departmentDetails.departmentImage} className="rounded-lg" />
                     </div>
-                    <div className="justify-center flex">
-                    <p className=" text-cyan-900 text-lg font-medium">This is one of the popular treatment in our clinic . {departmentDetails.departmentDiscription}</p>
-                    </div>
-                    <div className="justify-start flex mt-4">
+
+                    <p className=" text-cyan-900   text-lg  font-medium">This is one of the popular treatment in our clinic . {departmentDetails.departmentDiscription}</p>
+
+                    <div className="justify-start  flex mt-4">
                     <button onClick={()=>navigate('/doctors',{state:{departmentData: departmentDetails.departmentName}})} class="bg-cyan-800 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-2xl">
                     Find Doctors
                    </button>
